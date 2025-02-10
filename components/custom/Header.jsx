a5ced70d015be0384f4e6f79888c55b0326cb883
DhiravPatel24 <dhirav.patel@iflair.com>
2025-02-10 14:05:36 +0530

    changes

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const{userDetail, setUserDetail} = useContext(UserDetailContext);
   return (
     <div className='p-4 flex justify-between items-center'>
-     <Image src={'/logo.jpg'} alt="logo" width={50} height={50} />
+     <Image src={'/logo1.png'} alt="logo" width={100} height={100} />
      {!userDetail?.name &&
      <div className='flex gap-5'>
         <Button variant='ghost'>Sign In</Button>
